66c8fbb718aac92519dbb7a1a49430c868104128
cruiseure27 <manunush@free.fr>
2016-01-04 14:10:30 +0100

    Ajout d'un mapping sur "/"

--- sportBooking/src/main/java/isen/sportBooking/Application.java
+++ sportBooking/src/main/java/isen/sportBooking/Application.java
@@ -3,16 +3,14 @@ package isen.sportBooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class })
+@EnableAutoConfiguration
+@RestController
 public class Application extends SpringBootServletInitializer {
 
 	@Override
@@ -24,5 +22,11 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@RequestMapping("/")
+	String hello(){
+		return "Hello Maggle";
+	}
+
 
 }
