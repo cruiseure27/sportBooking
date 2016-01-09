1d42d7bc321538afbd616e78391d391d8e568ee4
cruiseure27 <manunush@free.fr>
2016-01-09 12:18:21 +0100

    Commit de victoire d'ajout du controlleur de vue homme

--- sportBooking/src/main/java/isen/sportBooking/controller/HomeController.java
+++ sportBooking/src/main/java/isen/sportBooking/controller/HomeController.java
@@ -2,13 +2,13 @@ package isen.sportBooking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
-	String getView(){
-		return "Hello";
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getHome() {
+		return "home";
 	}
-
 }
